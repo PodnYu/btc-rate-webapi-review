ec07384df4333545f68cbbb99a01e1965d5232b7
Yuriy Podnebesov <dartwader032@gmail.com>
2021-08-10 19:47:40 +0300

    changed default body for ResponseDto

--- src/api/dtos/ResponseDto.ts
+++ src/api/dtos/ResponseDto.ts
@@ -2,7 +2,7 @@ export class ResponseDto {
 	status: number;
 	body: any;
 
-	constructor(status = 200, body = null) {
+	constructor(status = 200, body = {}) {
 		this.status = status;
 		this.body = body;
 	}
